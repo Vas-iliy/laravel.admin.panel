a67ec42aea1628b47044c45d329f5101318c4753
Vasiliy <kkolyasev1228@gmail.com>
2020-08-20 22:04:06 +0300

    responce

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Resource_;
 
 class AboutController extends Controller
 {
     public function show () {
-        $title = 'Привет пидор';
+        $title = 'Hi';
         if (view()->exists('defoult.about')) {
+
+            //$view = view('defoult.about', compact('title'));
+            /*return (new Response($view))->header('Content-Type', 'New Type');*/
+
+            //return response()->json(['name' => 'Hello']);
+
+            //return response()->view('defoult.about', compact('title'));
+
+            //return response()->download('robots.txt', 'nax.txt');
+
+
             return view('defoult.about', compact('title'));
         }
+        abort(404);
     }
 
 }
