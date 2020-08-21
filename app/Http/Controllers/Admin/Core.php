210ee86c30466a165b2db5ca09b0034f27168990
Vasiliy <kkolyasev1228@gmail.com>
2020-08-21 22:53:05 +0300

    sql

--- app/Http/Controllers/Admin/Core.php
+++ app/Http/Controllers/Admin/Core.php
@@ -4,13 +4,88 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Core extends Controller
 {
+
     /**
      * list materials
      */
     protected function getArticles () {
+
+        //вся информация
+        //$articles = DB::table('articles')->get();
+
+        //первая запись
+        //$articles = DB::table('articles')->first();
+
+        //информация по полю
+        //$articles = DB::table('articles')->value('name');
+
+        //все записи по полю
+        //$articles = DB::table('articles')->pluck('name');
+
+        //кол-во записай таблицы
+        //$articles = DB::table('articles')->count();
+
+        //максимальное значение
+        //$articles = DB::table('articles')->max('id');
+
+        //выбираем по полям
+        //$articles = DB::table('articles')->select('name', 'id', 'text')->get();
+
+        //уникальные значения
+        //$articles = DB::table('articles')->distinct()->select('name')->get();
+
+        //добавление полей
+        //$query = DB::table('articles')->select('name');
+        //////////////////////////////////////////////////////////////////kod
+        //$articles = $query->addSelect('text')->get();
+
+        //WHERE
+        //$articles = DB::table('articles')->select('text')->where('id', '<=', '2')->get();
+
+
+        /*$articles = DB::table('articles')->select('text', 'name')
+                                         ->where('id', '>', '4')
+                                         ->where('name', 'like', 'test%', 'or')
+                                         ->get();*/
+        //между элементами
+        //$articles = DB::table('articles')->whereBetween('id', [1, 3])->get();
+
+        //количество записей из БД с пропуском
+        //$articles = DB::table('articles')->take(3)->skip(2)->get();
+
+
+        /*DB::table('articles')->insert(
+                                                            [
+                                                                [
+                                                                    'name' => 'TEST',
+                                                                    'text' => 'lalala',
+                                                                    'img' => '1020'
+                                                                ],
+                                                                [
+                                                                    'name' => 'TEST10100',
+                                                                    'text' => 'lalala',
+                                                                    'img' => '2020'
+                                                                ]
+                                                            ]
+                                                        );*/
+
+        /*$result = DB::table('articles')->insertGetId(
+                [
+                    'name' => 'TEST',
+                    'text' => 'lalala',
+                    'img' => '1020'
+                ]
+        );*/
+
+        //DB::table('articles')->where('id',  '=', 2)->update(['name' => 'Hi']);
+
+        //$result = DB::table('articles')->where('id', '>', 10)->delete();
+
+        //dump($result);
 
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use mysql_xdevapi\Schema;
 
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+
+        DB::listen(function ($query) {
+            //dump($query->sql);
+        });
     }
 }
