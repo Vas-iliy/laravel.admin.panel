21ad00e456674fc4004cf92f2ec4d31f61388d6f
Vasiliy <kkolyasev1228@gmail.com>
2020-08-21 17:58:14 +0300

    refactor

--- app/Http/Controllers/Admin/DifferentController.php
+++ app/Http/Controllers/Admin/DifferentController.php
@@ -11,9 +11,21 @@ class DifferentController extends Controller
     public function show () {
         $title = 'Different';
         if (view()->exists('defoult.different')) {
+
+            //DB::insert("INSERT INTO articles (`name`, `text`, `img`) VALUES (?, ?, ?)", ['test 1', 'text', 'img']);
+
+            //$result = DB::connection()->getPdo()->lastInsertId();
+
+            //$result = DB::update("UPDATE articles SET `name` = ? WHERE `id` = ?", ['TEST X', 5]);
+
+            //DB::delete("DELETE FROM articles WHERE `id` = ?", [5]);
+
+            //DB::statement('DROP table articles');
             $articles = DB::select('SELECT * FROM articles ');
 
+            //dump($result);
             dump($articles);
+
 
             return response()->view('defoult.different', compact('title'));
         }
