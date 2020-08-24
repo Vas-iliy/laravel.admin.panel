d817ede7a45dd211e4c02813e0f3ce9dcd75c53a
Vasiliy <kkolyasev1228@gmail.com>
2020-08-24 17:16:38 +0300

    Role

--- database/migrations/2020_08_24_141105_create_role_user_table.php
+++ database/migrations/2020_08_24_141105_create_role_user_table.php
@@ -15,6 +15,13 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->bigInteger('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles');
+
             $table->timestamps();
         });
     }
